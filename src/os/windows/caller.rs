e21db405c2dbdb10c5ca6f198f4cd441d19c5428
Shinwoo Park <natural7530@gmail.com>
2021-03-24 00:05:05 +0900

    test: rename `load_caller()` to `test_load_caller()`

--- src/os/windows/caller.rs
+++ src/os/windows/caller.rs
@@ -462,7 +462,7 @@ mod tests {
     use std::sync::Arc;
 
     #[async_std::test]
-    async fn load_caller() -> Result<(), Box<dyn std::error::Error>> {
+    async fn test_load_caller() -> Result<(), Box<dyn std::error::Error>> {
         let caller = Arc::new(Caller::new(None)?);
         println!("api_path: {:?}", caller.handle().read().await.get_api_path().await);
         assert!(!caller.handle().read().await.is_connected().await);
