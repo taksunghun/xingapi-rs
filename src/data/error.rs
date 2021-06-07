efabab7661f75c40df875eb9e7b20adec14239d2
Shinwoo Park <natural7530@gmail.com>
2021-06-08 01:10:11 +0900

    docs: update error docs in `data::error`

--- src/data/error.rs
+++ src/data/error.rs
@@ -2,20 +2,20 @@
 
 //! [`Data`](crate::data::Data)를 인코딩하거나 디코딩할 때 발생하는 에러에 대한 모듈입니다.
 //!
-//! 이러한 에러들은 RES 파일이 오래되어 서버와 달라 발생할 수도 있습니다.
+//! 이러한 에러들은 다운로드 받은 RES 파일이 오래되어 서버와 달라 발생할 수도 있습니다.
 
 /// 응답 데이터를 디코딩할 때 발생하는 에러입니다.
 #[derive(Clone, Debug)]
 pub enum DecodeError {
     /// TR 코드를 불러오지 않았거나 알 수 없습니다.
     UnknownTrCode,
-    /// block이 누락되었습니다.
+    /// 블록이 누락되었습니다.
     UnknownBlockName { block_name: String },
-    /// 데이터 길이가 맞지 않습니다.
+    /// 데이터 크기가 일치하지 않습니다.
     MismatchBufferLength,
-    /// occurs(배열) 길이를 디코딩할 수 없습니다.
+    /// 배열 크기를 디코딩할 수 없습니다.
     DecodeOccursLength,
-    /// CP949 데이터를 UTF-8로 디코딩할 수 없습니다.
+    /// CP949 데이터를 디코딩할 수 없습니다.
     DecodeString,
 }
 
@@ -28,30 +28,28 @@ impl std::fmt::Display for DecodeError {
             }
             Self::MismatchBufferLength => write!(f, "mismatch buffer length"),
 
-            Self::DecodeOccursLength => write!(f, "could not decode occurs length"),
-            Self::DecodeString => write!(f, "could not decode string"),
+            Self::DecodeOccursLength => write!(f, "could not decode array length"),
+            Self::DecodeString => write!(f, "could not decode cp949 string"),
         }
     }
 }
 
 impl std::error::Error for DecodeError {}
 
-/// 데이터를 인코딩할 때 발생하는 에러입니다.
+/// 요청 데이터를 인코딩할 때 발생하는 에러입니다.
 #[derive(Clone, Debug)]
 pub enum EncodeError {
     /// TR 코드를 불러오지 않았거나 알 수 없습니다.
     UnknownTrCode,
-    /// block이 누락되었습니다.
+    /// 블록이 누락되었습니다.
     MissingBlock { block_name: String },
-    /// block 타입이 일치하지 않습니다.
+    /// 블록 타입이 일치하지 않습니다.
     MismatchBlockType { block_name: String },
-    /// block 최대 개수를 초과했습니다.
-    ///
-    /// 개수는 5자리 이하의 자연수여야 합니다.
+    /// 배열의 최대 크기에 도달했습니다. 배열의 크기는 5자리 이하여야 합니다.
     ExceedMaxBlockCount { block_name: String },
-    /// field가 누락되었습니다.
+    /// 필드가 누락되었습니다.
     MissingField { block_name: String, field_name: String },
-    /// field의 최대 길이를 초과했습니다.
+    /// 필드의 최대 크기에 도달했습니다.
     ExceedFieldLength { block_name: String, field_name: String },
 }
 
