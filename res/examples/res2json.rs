84f9383d118ac8842bb2b4aed3695dc8ada7bcd2
Shinwoo Park <natural7530@gmail.com>
2021-08-10 03:40:57 +0900

    Use `Path` instead of `str` in res2json example

--- res/examples/res2json.rs
+++ res/examples/res2json.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 use clap::{App, Arg};
-use std::fs::OpenOptions;
+
+use std::fs::{self, OpenOptions};
+use std::path::Path;
 
 fn main() {
     let matches = App::new("res2json")
@@ -10,8 +12,8 @@ fn main() {
         .arg(Arg::with_name("pretty").short("p"))
         .get_matches();
 
-    let input = matches.value_of("input");
-    let output = matches.value_of("output").unwrap();
+    let input = matches.value_of("input").map(|i| Path::new(i));
+    let output = matches.value_of("output").map(|i| Path::new(i)).unwrap();
     let pretty = matches.is_present("pretty");
 
     let tr_layouts = if let Some(path) = input {
@@ -30,5 +32,5 @@ fn main() {
         serde_json::to_writer(&file, &tr_layouts).unwrap();
     }
 
-    println!("json dumped: {}", output);
+    println!("json encoded: \"{}\"", fs::canonicalize(output).unwrap().display());
 }
