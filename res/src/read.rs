61f9fc4eacf5dfea9d2d181027951e168a9aa043
Shinwoo Park <natural7530@gmail.com>
2021-08-10 17:52:33 +0900

    refactor: simplify iteration in `StrRead`

--- res/src/read.rs
+++ res/src/read.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{cell::RefCell, str::CharIndices};
+use std::{cell::RefCell, iter::Peekable, str::CharIndices};
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Position {
@@ -29,7 +29,7 @@ pub struct StrRead<'a> {
 }
 
 pub struct StrReadState<'a> {
-    iter: CharIndices<'a>,
+    iter: Peekable<CharIndices<'a>>,
     latest_offset: usize,
 }
 
@@ -37,12 +37,15 @@ impl<'a> StrRead<'a> {
     pub fn new(string: &'a str) -> Self {
         Self {
             string,
-            state: RefCell::new(StrReadState { iter: string.char_indices(), latest_offset: 0 }),
+            state: RefCell::new(StrReadState {
+                iter: string.char_indices().peekable(),
+                latest_offset: 0,
+            }),
         }
     }
 
     fn skip_until_not_whitespace(state: &mut StrReadState) -> Option<()> {
-        while let Some((_, ch)) = state.iter.clone().next() {
+        while let Some((_, ch)) = state.iter.peek() {
             match ch {
                 ' ' | '\t' | '\r' | '\n' => {
                     state.iter.next().unwrap();
@@ -57,14 +60,11 @@ impl<'a> StrRead<'a> {
     }
 
     fn skip_until_not_comment(state: &mut StrReadState) -> Option<()> {
-        #[allow(clippy::while_let_on_iterator)]
         while let Some((_, ch)) = state.iter.next() {
             if ch == '*' {
-                if let Some((_, ch)) = state.iter.clone().next() {
-                    if ch == '/' {
-                        state.iter.next().unwrap();
-                        return Some(());
-                    }
+                if let Some((_, '/')) = state.iter.peek() {
+                    state.iter.next().unwrap();
+                    return Some(());
                 }
             }
         }
@@ -85,38 +85,37 @@ impl<'a> Read<'a> for StrRead<'a> {
     fn next_symbol(&self) -> Option<&'a str> {
         let state = &mut *self.state.borrow_mut();
 
-        StrRead::skip_until_not_whitespace(state)?;
-        state.latest_offset = state.iter.clone().next()?.0;
+        Self::skip_until_not_whitespace(state)?;
+        state.latest_offset = state.iter.peek()?.0;
         let mut start_idx = state.latest_offset;
         let mut end_idx = self.string.len();
 
-        while let Some((idx, ch)) = state.iter.clone().next() {
+        while let Some(&(idx, ch)) = state.iter.peek() {
             match ch {
                 '\r' | '\n' => {
                     if idx == start_idx {
-                        StrRead::skip_until_not_whitespace(state)?;
-                        state.latest_offset = state.iter.clone().next()?.0;
+                        Self::skip_until_not_whitespace(state)?;
+
+                        state.latest_offset = state.iter.peek()?.0;
                         start_idx = state.latest_offset;
+                        continue;
                     } else {
                         end_idx = idx;
                         break;
                     }
                 }
                 '/' => {
-                    if let Some((_, ch2)) = state.iter.clone().nth(1) {
-                        if ch2 == '*' {
-                            if idx == start_idx {
-                                state.iter.next().unwrap();
-                                state.iter.next().unwrap();
-                                StrRead::skip_until_not_comment(state)?;
+                    if let Some((_, '*')) = state.iter.clone().nth(1) {
+                        if idx == start_idx {
+                            state.iter.nth(1).unwrap();
+                            Self::skip_until_not_comment(state)?;
 
-                                state.latest_offset = state.iter.clone().next()?.0;
-                                start_idx = state.latest_offset;
-                                continue;
-                            } else {
-                                end_idx = idx;
-                                break;
-                            }
+                            state.latest_offset = state.iter.peek()?.0;
+                            start_idx = state.latest_offset;
+                            continue;
+                        } else {
+                            end_idx = idx;
+                            break;
                         }
                     }
 
@@ -125,7 +124,7 @@ impl<'a> Read<'a> for StrRead<'a> {
                 ',' | ';' => {
                     if idx == start_idx {
                         state.iter.next().unwrap();
-                        end_idx = if let Some((idx2, _)) = state.iter.clone().next() {
+                        end_idx = if let Some(&(idx2, _)) = state.iter.peek() {
                             idx2
                         } else {
                             self.string.len()
@@ -149,7 +148,7 @@ impl<'a> Read<'a> for StrRead<'a> {
         let state = &*self.state.borrow();
 
         let mut pos = Position { line: 1, column: 1 };
-        let mut iter = self.string.char_indices();
+        let mut iter = self.string.char_indices().peekable();
 
         while let Some((idx, ch)) = iter.next() {
             if idx >= state.latest_offset {
@@ -159,12 +158,8 @@ impl<'a> Read<'a> for StrRead<'a> {
             match ch {
                 // CR
                 '\r' => {
-                    if let Some((_, ch2)) = iter.clone().next() {
-                        // LF
-                        if ch2 == '\n' {
-                            iter.next().unwrap();
-                        }
-                    }
+                    iter.next_if(|&(_, ch2)| ch2 == '\n');
+
                     pos.line += 1;
                     pos.column = 1;
                 }
